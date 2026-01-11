ee7f3ef3bdebd4108409c404d1a7e4e3d08c489c
Lane Thompson <amerten908@hotmail.com>
2026-01-10 23:11:42 -0500

    lots of new stuff

--- main.py
+++ main.py
@@ -14,32 +14,36 @@ import os
 import pyfiglet
 
 beat = 1
+measure = 0
 
 def play():
     global beat
+    global measure
     if beat == 1:
-        winsound.Beep(500, 100)
+        winsound.Beep(500, 75)
     else:
-        winsound.Beep(400, 100)
-    sleep(delay_between_beeps - 0.01)
+        winsound.Beep(400, 75)
+    sleep(delay_between_beeps - 0.0075)
     if beat == int(float(timesig)):
         beat = 1
+        measure = measure + 1
     else:
         beat = beat + 1
     render()
     
 def render():
     os.system("cls")
-    print(Fore.MAGENTA + "Current Beat")
+    print(Fore.MAGENTA + "Current beat")
     if beat == 1:
         print(Fore.RED)
     elif beat % 2:
         print(Fore.GREEN)
     else:
         print(Fore.BLUE)
+    print(Style.BRIGHT + "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰")
+    print(pyfiglet.figlet_format(str(beat), font='slant') + "out of " + str(timesig) + " beat(s)")
     print("▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰")
-    print(Style.BRIGHT + pyfiglet.figlet_format(str(beat), font='slant') + "out of " + str(timesig) + " beats")
-    print("▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰")
+    print(Fore.WHITE + Style.NORMAL + "You are on measure " + str(measure))
     print(Fore.RED + "Press CTRL+C to Quit")
     play()
 
@@ -54,9 +58,18 @@ print("""
 |_____||_____|'.__.' |_____|      `.__.'    |_____|[___||__]'.__.' [___||__||__]'.__.'
 """)
 print(Fore.MAGENTA + "https://flavortown.hackclub.com/projects/7404")
+print(Fore.WHITE + "made for flavortown by hackclub!!")
 print(Fore.GREEN + "a simple metronome TUI app" + Back.YELLOW)
+winsound.Beep(700, 100)
+winsound.Beep(500, 100)
+winsound.Beep(600, 100)
+winsound.Beep(400, 100)
 bpm = input(Fore.BLACK + "Enter BPM: ")
+winsound.Beep(400, 100)
+winsound.Beep(600, 100)
 timesig = input("Enter NPM (Notes Per Measure): ")
+winsound.Beep(400, 100)
+winsound.Beep(600, 100)
 delay_between_beeps = (60 / int(float(bpm)))
 print(Style.RESET_ALL)
 render()
